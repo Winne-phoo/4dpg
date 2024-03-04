76b3fc99bdfb7686dfaac054b4c389587f17e54b
winne phoo <160997902+Winne-phoo@users.noreply.github.com>
2024-03-04 15:15:09 -0800

    Update proyecto4DPGM.php

--- proyecto4DPGM.php
+++ proyecto4DPGM.php
@@ -3,7 +3,7 @@ $warzon =[
   "capacidad de birgenes" => "n+50"
   "tamaño de mapa" => "9 km cuadrados"
   "numero de ubicasiones" => 17
-  "cauntos maps tiene" => 4
+  "cauntos maps tiene" => 5
   "cuantos modos de juego tiene" => 2
 ]
   
