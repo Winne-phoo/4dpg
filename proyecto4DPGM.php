5a357f417b583aac3ffd600518716f8add122ab7
alfredoEm225 <161801572+alfredoEm225@users.noreply.github.com>
2024-03-04 15:14:47 -0800

    Update proyecto4DPGM.php
    se agrega
     warzon

--- proyecto4DPGM.php
+++ proyecto4DPGM.php
@@ -1,2 +1,9 @@
 <?php
-$person
+$warzon =[
+  "capacidad de birgenes" => "n+50"
+  "tamaño de mapa" => "9 km cuadrados"
+  "numero de ubicasiones" => 17
+  "cauntos maps tiene" => 4
+  "cuantos modos de juego tiene" => 2
+]
+  
