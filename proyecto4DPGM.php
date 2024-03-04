14a47432be9bf79cea5fc40d8f75b9f7722cba91
winne phoo <160997902+Winne-phoo@users.noreply.github.com>
2024-03-04 15:44:09 -0800

    Update proyecto4DPGM.php
    agrega fornite

--- proyecto4DPGM.php
+++ proyecto4DPGM.php
@@ -1,9 +1,14 @@
 <?php
 $warzon =[
   "capacidad de birgenes" => "n+50"
-  "tamaño de mapa" => "9 km cuadrados"
-  "numero de ubicasiones" => 17
-  "cauntos maps tiene" => 5
-  "cuantos modos de juego tiene" => 2
+  "tamaño de mapa" => 9,
+  "numero de ubicasiones" => 17,
+  "cauntos maps tiene" => 5,
+  "cuantos modos de juego tiene" => 2,
 ]
-  
+  $fornite[
+  "capacida de jugadores" => "n100"
+  "tamaño de mapa" => 5.4
+  "numero de ubicasiones" => 14,
+  ""
+  ]
