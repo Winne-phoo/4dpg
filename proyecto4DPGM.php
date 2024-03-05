1fa067eee586b54b2ecb0ce29d0889efb4f17b8b
winne phoo <160997902+Winne-phoo@users.noreply.github.com>
2024-03-05 00:27:33 -0600

    Update proyecto4DPGM.php

--- proyecto4DPGM.php
+++ proyecto4DPGM.php
@@ -1,14 +1,31 @@
 <?php
 $warzon =[
   "capacidad de birgenes" => "n+50"
-  "tamaño de mapa" => 9,
+  "tamaño de mapa" => 9.5,
   "numero de ubicasiones" => 17,
   "cauntos maps tiene" => 5,
   "cuantos modos de juego tiene" => 2,
 ]
   $fornite[
-  "capacida de jugadores" => "n100"
-  "tamaño de mapa" => 5.4
+  "capacida de jugadores" => "n100",
+  "tamaño de mapa" => 5.4,
   "numero de ubicasiones" => 14,
-  ""
+  "cuantos mapas tiene" => 1,
+  "cuantos modos tiene" => 3,
+  "en que año enpesaron" => "21 de julio de 2017"
+  "nombre de el creador o empresa" => "epic games"
+  "dinero total ganado" => "5.600 millones de dolares"
+  "total de jugadores" => "400 millones de jugadore registrados"
+  "que tipos de zonas tienen" => "tormentas"
+  "como se llega al batle royal" => "en un camion"
+  "como ganas escudo" => "con bebidas"
+  "cantidad de vida" => 100
+  "cantidad de escudo" => 150
+  "numero de personas que pueden jugar" => 4
+  "tiempo de inicio aprox de una partida" => "1 m.n"
+  "cantidad de servidores" => 6
+  "cuanto gana al dia" =>6 "millones de dolares"
+  "tipos de combates" => "todo tipo"
+  "tipos de armas" => "normales"
+  "en donde se puede jugar" => "pc xbox playstation switch"
   ]
