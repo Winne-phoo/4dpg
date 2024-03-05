c32b9253e99f82f9c49a56cae6ac4d4dffae89bc
luison784 <161802146+luison784@users.noreply.github.com>
2024-03-05 01:05:56 -0600

    Update proyecto4DPGM.php

--- proyecto4DPGM.php
+++ proyecto4DPGM.php
@@ -1,6 +1,6 @@
 <?php
 $warzon =[
-  "capacidad de birgenes" => "n+50",
+  "capacidad de jugadores" => "n+50",
   "tamaño de mapa" => 9.5,
   "numero de ubicasiones" => 17,
   "cauntos maps tiene" => 5,
@@ -46,8 +46,8 @@ $warzon =[
   "en donde se puede jugar" => "pc xbox playstation switch"
   ];
   $freefire =[
-  "capacidad de birgenes" => 100,
-  "tamaño de mapa" => 10,
+  "capacidad de jugadores" => 100,
+  "tamaño de mapa" => 10.5,
   "numero de ubicasiones" => 15,
   "cauntos maps tiene" => 6,
   "cuantos modos de juego tiene" => 3,
@@ -68,3 +68,27 @@ $warzon =[
   "tipos de armas" => "normales",
  "en donde se puede jugar" => "pc telefono",
   ];
+  $apex =[
+  "capacidad de jugadores" => 100,
+  "tamaño de mapa" => 7.5,
+  "numero de ubicasiones" => 15,
+  "cauntos maps tiene" => 3,
+  "cuantos modos de juego tiene" => 1,
+  "en que año enpesaron" => 2019,
+  "nombre de el creador o empresa" => "Respawn Entertainment ",
+  "dinero total ganado" => "2.000 millones de dólares",
+  "total de jugadores" => "600.000 jugadores",
+  "que tipos de zonas tienen" => "tormenta roja",
+  "como se llega al batle royal" => "por una tirolesa",
+  "como ganas escudo" => "baterias",
+  "cantidad de vida" => 100,
+  "cantidad de escudo" => 150,
+  "numero de personas que pueden jugar" => 4,
+  "tiempo de inicio aprox de una partida" => "varia",
+  "cantidad de servidores" => 6,
+  "cuanto gana al dia" =>"25 millones",
+  "tipos de combates" => "todo tipo",
+  "tipos de armas" => "laser",
+ "en donde se puede jugar" => "pc xbox play",
+  ];
+
