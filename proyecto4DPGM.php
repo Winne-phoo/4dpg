7293c698bab60f2ebfe455b1204cebc629ea8d46
alfredoEm225 <161801572+alfredoEm225@users.noreply.github.com>
2024-03-05 00:43:08 -0600

    Update proyecto4DPGM.php

--- proyecto4DPGM.php
+++ proyecto4DPGM.php
@@ -1,10 +1,26 @@
-<?php
+u<?php
 $warzon =[
-  "capacidad de birgenes" => "n+50"
+  "capacidad de birgenes" => "n+50",
   "tamaño de mapa" => 9.5,
   "numero de ubicasiones" => 17,
   "cauntos maps tiene" => 5,
   "cuantos modos de juego tiene" => 2,
+  "en que año enpesaron" => 2003,
+  "nombre de el creador o empresa" => "Gray Matter Interactive",
+  "dinero total ganado" => "390 millones de dólares ",
+  "total de jugadores" => "250.000 y 430.000 jugadores",
+  "que tipos de zonas tienen" => "gas mostasa",
+  "como se llega al batle royal" => "en un avion",
+  "como ganas escudo" => "con placas de blindaje",
+  "cantidad de vida" => 100,
+  "cantidad de escudo" => 150,
+  "numero de personas que pueden jugar" => 4,
+  "tiempo de inicio aprox de una partida" => "varia",
+  "cantidad de servidores" => 6,
+  "cuanto gana al dia" =>"225 millones",
+  "tipos de combates" => "todo tipo",
+  "tipos de armas" => "normales",
+  "en donde se puede jugar" => "pc xbox playstation telefono",
 ]
   $fornite[
   "capacida de jugadores" => "n100",
